94fc9f267227a6d6195937442707d6b2ee3a329f
Aderson Alves <adersonrs@gmail.com>
2018-09-28 22:52:56 -0300

    Inicio do Site, propriamente dito.

--- controle/homeCtl.php
+++ controle/homeCtl.php
@@ -1,8 +1,18 @@
 <?php
-	 class homeCtl extends controller{
-		 public function index(){
-		 	$this->loadView('index');
-		}
+class homeCtl extends controller{
+	
+	public function index(){
+		session_start();
+		if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true)){
+			
+			echo "ERRO";
+			header("location: logar");
+		}else{
+			$logado = $_SESSION['login'];
+			$this->loadView('index');
+		}	
 	}
+	
+}
 
- ?>
+?>
--- modelo/DAO/Banco.php
+++ modelo/DAO/Banco.php
@@ -5,7 +5,7 @@ class Banco{
 	protected $array;
 	public function __construct($host = "localhost", $dbname = "gdee", $dbuser = "root", $dbpass = ""){
 		try {
-			$this->pdo = new PDO("mysql:dbname=".$dbname.";host=".$host, $dbuser, $dbpass);
+			$this->pdo = new PDO("mysql:dbname=".$dbname.";host=".$host."; charset=utf8", $dbuser, $dbpass);
 		} catch (PDOException $e) {
 			echo "Falhou:".$e->getMessage();
 		}
